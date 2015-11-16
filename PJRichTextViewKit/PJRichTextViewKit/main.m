0fb47dcd9560a3216e2eec787c0b53b4a9afd027
JessicaYang <441914317@qq.com>
2015-11-16 15:36:14 +0800

    添加main文件
    添加main文件

---
+++ PJRichTextViewKit/PJRichTextViewKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PJRichTextViewKit
+//
+//  Created by yangjuanping on 15/11/16.
+//  Copyright © 2015年 yangjuanping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
